f0d556b3eae91378ae7ddca1f06a35dae8103a1c
hussain.a121145 <140621473+hussainalamin701@users.noreply.github.com>
2023-08-23 18:33:46 +0100

    I have added more javascript and nearly finished off my project

---
+++ javascript.js
@@ -0,0 +1,112 @@
+const playerScorePara = document.getElementById("playerScore");
+let playerScoreVal = 0;
+const computerScorePara = document.getElementById("computerScore");
+let computerScoreVal = 0;
+
+const scissorBtn = document.getElementById("Scissor");
+const paperBtn = document.getElementById("Paper");
+const rockBtn = document.getElementById("Rock");
+
+const player_icon = document.getElementById("player-icon");
+const player_choice = document.getElementById("player-choice");
+const computer_icon = document.getElementById("computer-icon");
+const computer_choice = document.getElementById("computer-choice");
+
+
+const buttons = document.querySelectorAll(".btn");
+
+let computerSelection = ['Rock','Paper','Scissor'];
+
+let temp_comp_choice = '';
+let temp_play_choice = '';
+// Make the computers choice of value
+
+playerScorePara.innerHTML = `Player Score : ${playerScoreVal}`;
+computerScorePara.innerHTML = `Computer Score : ${computerScoreVal}`;
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function computerPlay(computerChoice){
+    let choiceNum = Math.floor(Math.random() * 3);
+    if(choiceNum === 0){
+        computerChoice = 'Rock';
+    }else if(choiceNum === 1){
+        computerChoice = 'Paper';
+    }else if(choiceNum === 2){
+        computerChoice = 'Scissor';
+    }
+    return computerChoice;
+}
+
+function checkWinner(){
+    if(playerScoreVal === 5 || computerScoreVal === 5){
+        console.log("Won Overall");
+    }
+}
+
+function compare(playerSelection, computerSelection){
+    if(playerSelection === computerSelection){
+        document.getElementById("win-lose-text").innerHTML = "Tie";
+        console.log("Tie");
+    }
+
+    if(
+        (playerSelection == "Rock" && computerSelection == "Scissor") ||
+        (playerSelection == "Paper" && computerSelection == "Rock") ||
+        (playerSelection == "Scissor" && computerSelection == "Paper")
+        ){
+        document.getElementById("win-lose-text").innerHTML = "Player Wins";
+        playerScoreVal++;
+        
+        playerScorePara.innerHTML = `Player Score : ${playerScoreVal}`;
+    }
+
+    if(
+        (playerSelection == "Scissor" && computerSelection == "Rock") ||
+        (playerSelection == "Rock" && computerSelection == "Paper") ||
+        (playerSelection == "Paper" && computerSelection == "Scissor")
+        ){
+        document.getElementById("win-lose-text").innerHTML = "Computer Wins";
+        computerScoreVal++;
+        
+        computerScorePara.innerHTML = `Computer Score : ${computerScoreVal}`;
+    }
+}
+
+function buttonRock(){
+    let abx = computerPlay(temp_comp_choice);
+    document.getElementById("computer-choice").innerHTML = `Computer Chose : ${abx}`;
+    temp_play_choice = document.getElementById("Rock").id;
+    document.getElementById("player-choice").innerHTML = `Player Chose : ${temp_play_choice}`
+
+    compare(temp_play_choice, abx);
+    checkWinner();
+}
+
+function buttonPaper(){
+    let abx = computerPlay(temp_comp_choice);
+    document.getElementById("computer-choice").innerHTML = `Computer Chose : ${computerPlay(temp_comp_choice)}`;
+    temp_play_choice = document.getElementById("Paper").id;
+    document.getElementById("player-choice").innerHTML = `Player Chose : ${temp_play_choice}`
+   
+    compare(temp_play_choice, abx);
+    checkWinner();
+}
+
+function buttonScissor(){
+    let abx = computerPlay(temp_comp_choice);
+    
+    document.getElementById("computer-choice").innerHTML = `Computer Chose : ${abx}`;
+    temp_play_choice = document.getElementById("Scissor").id;
+    document.getElementById("player-choice").innerHTML = `Player Chose : ${temp_play_choice}`
+
+    compare(temp_play_choice, abx);
+    checkWinner();
+
+}
+
+rockBtn.addEventListener("click", buttonRock);
+paperBtn.addEventListener("click", buttonPaper);
+scissorBtn.addEventListener("click", buttonScissor);
